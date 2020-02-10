6177a8ce0b61b48abacbd19ae4b9851788027327
Alexis Perignon <alexis.perignon@etu.u-bordeaux.fr>
2020-02-10 14:29:28 +0100

    Add tools astar: board and node

---
+++ src/prototypes/astar-prototype/node.py
@@ -0,0 +1,45 @@
+class Node:
+
+    def __init__(self, x, y, cellContent):
+        self.x = x
+        self.y = y
+        self.cellContent = cellContent
+        self.cellCost = self.setCellCosts()
+        self.cost = 0
+        self.estimatedCost = 0
+        self.neighbors = []
+        self.parent = None
+    
+    def addNeighbor(self, node):
+        self.neighbors.append(node)
+
+    def __lt__(self, other):
+        return self.estimatedCost < other.estimatedCost
+
+    def __eq__(self, other):
+        return (self.x == other.x and self.y == other.y)
+
+    # Get cell cost based on content
+    def setCellCosts(self):
+        # Water
+        if self.cellContent is 'w':
+            return 100
+
+        # Mountains
+        if self.cellContent is 'm':
+            return 50
+
+        # Forests
+        if self.cellContent is 'f':
+            return 10
+
+        # Grassland
+        if self.cellContent is 'g':
+            return 5
+
+        # Roads
+        if self.cellContent is 'r':
+            return 1
+
+        # Default
+        return 1

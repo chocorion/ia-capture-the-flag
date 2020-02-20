224745200558fdc8649bd5762120dc0ac5ca70f6
Alexis Perignon <alexis.perignon@etu.u-bordeaux.fr>
2020-02-20 20:20:26 +0100

    small fix about how we increment index

--- src/game/ai/playerTest/myPlayerAstar.py
+++ src/game/ai/playerTest/myPlayerAstar.py
@@ -139,14 +139,14 @@ class myPlayer(Player):
     """
 
     def incrementIndex(self, bot_id, current_position):
-        maxDistance = 80.0
+
         if bot_id in self._currentPath and bot_id in self._currentIndex and self._currentPath[bot_id] != None :
             
             posInPath    = self.getNextPos(bot_id)
             lengthPath   = len(self._currentPath[bot_id]) -1
             distancePath = self._distance(current_position, posInPath)
-
-            if distancePath < maxDistance and self._currentIndex[bot_id] != lengthPath :
+            distanceOld  = self._distance(current_position,self._lastPosition[bot_id])
+            if distanceOld - distancePath > -35 and self._currentIndex[bot_id] != lengthPath :
                 self._currentIndex[bot_id] += 1
             
 
@@ -175,7 +175,6 @@ class myPlayer(Player):
                 angle -= 360
 
             pos_x, pos_y = current_position[0], current_position[1]
-            
             # Bloqué à gauche
             if pos_x % Map.BLOCKSIZE < 3 and self._map["blocks"][x - 1][y].solid:
                 if angle < 0: #negative
@@ -261,7 +260,7 @@ class myPlayer(Player):
 
     def getReturnPoll(self):
         returnData  = { "bots": { } }
-        maxDistance = 10.0
+        maxDistance = 0.0
 
         for bot_id in self._pollingData["bots"].keys():
             current_position = self._pollingData["bots"][bot_id]["current_position"]

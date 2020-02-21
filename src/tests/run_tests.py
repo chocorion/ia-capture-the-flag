5137b9af32f7cc119a71f22e44ccc4f27eeee2a7
Robin Navarro <robin.navarro@etu.u-bordeaux.fr>
2020-02-21 15:52:34 +0100

    Correct test import

--- src/tests/run_tests.py
+++ src/tests/run_tests.py
@@ -2,7 +2,7 @@
 
 import unittest
 
-from behaviorTrees import TestBehaviorTree
+from behavior_trees import TestBehaviorTree
 from ArgBuilder import TestDictBuilder
 from PythonPhysics import TestPythonPhysics
 from GameModel import TestGameModel
